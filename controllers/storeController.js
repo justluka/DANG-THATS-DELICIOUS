185cf2f1669b42a853653a47725ba642e043769a
Luis Carranza <lukanew@Luiss-MacBook-Pro.local>
2017-11-05 18:43:36 -0500

    Module 9

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,16 +1,4 @@
-
-exports.myMiddleware=(req,res,next) =>{
-    req.name='wes';
-
-    if(req.name==='wes'){
-        throw Error('that is a stupid name');
-    }
-    next();
-}
-
-
 exports.homePage = (req,res) => {
     console.log(req.name);
-    res.render('index');
-
-}
+	res.render('index');
+};
