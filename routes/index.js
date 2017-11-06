f62d402306c44b0068db9124dea8a9a190936f84
Luis Carranza <lukanew@Luiss-MacBook-Pro.local>
2017-11-06 07:49:54 -0500

    Module 10.1

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require ('../controllers/storeController');
+const { catchErrors } = require ('../handlers/errorHandlers');
 
-// Do work here
-router.get('/', (req, res) => {
- const wes={name:'luis',age:100, cool:true};
-  //res.json(wes);
-  //res.send(req.query);
-   res.render('hello',{name:'wes',dog:'Hugo', title:'I love Food'});  
-});
-
-router.get('/reverse/:name',(req,res)=>{
-   res.send(req.params);
-});
-
-//someone was here from MAcOS -- esta es un nuevo test,
-//aprendiendo a usar github
-// this line was edited directly from github
+router.get('/', storeController.homePage);
+router.get('/add',storeController.addStore);
+router.post('/add',catchErrors(storeController.createStore));
 
 module.exports = router;
