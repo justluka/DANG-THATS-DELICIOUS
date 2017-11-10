e683bf1928abe50100fb7fa1be2d7546127e9e95
Luis Carranza <lukanew@Luiss-MacBook-Pro.local>
2017-11-09 23:18:34 -0500

    Module18

--- public/dist/App.bundle.js
+++ public/dist/App.bundle.js
@@ -63,7 +63,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 3);
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -124,8 +124,7 @@ exports.$ = $;
 exports.$$ = $$;
 
 /***/ }),
-/* 2 */,
-/* 3 */
+/* 2 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const storeController = require ('../controllers/storeController');
-const { catchErrors } = require ('../handlers/errorHandlers');
+const storeController = require('../controllers/storeController');
+const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', catchErrors(storeController.getStores));
-router.get('/stores', storeController.getStores);
-router.get('/add',storeController.addStore);
-router.post('/add',catchErrors(storeController.createStore));
-router.post('/add/:id',catchErrors(storeController.updateStore));
+router.get('/stores', catchErrors(storeController.getStores));
+router.get('/add', storeController.addStore);
+
+router.post('/add',
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.createStore)
+);
+
+router.post('/add/:id',
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.updateStore)
+);
 
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 
